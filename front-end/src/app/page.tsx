4f021c454810403540155aa4084608fccd28e66a
chetan chudasama <chetanchudasama2024@gmail.com>
2024-09-30 15:00:54 +0530

    setup fullstack-test-task

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex ">
+      <div className="w-60"><h1 className="text-2xl">hello</h1></div>
+      
+      <div><small>Menus</small></div>
+    </div>
+  );
+}
