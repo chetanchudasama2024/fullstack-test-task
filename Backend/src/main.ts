338baca65ed8d822673ee2f68172240489c3c35c
chetan chudasama <chetanchudasama2024@gmail.com>
2024-10-03 18:24:42 +0530

    Enable cors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const logger = new Logger('bootstrap');
 
   const config = new DocumentBuilder()
